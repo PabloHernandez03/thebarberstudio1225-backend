6e60762ca4f1902d46fc1602ba221f2cbcb31306
PabloHernandez03 <pablo.hernandez6287@alumnos.udg.mx>
2026-05-02 15:41:45 -0600

    fixed: error ipv6 connection

--- config/mailer.js
+++ config/mailer.js
@@ -1,16 +1,27 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
-    user: process.env.EMAIL_USER, // Tu correo de Gmail
-    pass: process.env.EMAIL_PASS  // Tu Contraseña de Aplicación de 16 letras
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
-// Verificamos que la conexión sea correcta
-transporter.verify().then(() => {
-  console.log('✅ Sistema de correos listo para enviar notificaciones');
-});
+// Verificamos que la conexión sea correcta, Y ATRAPAMOS EL ERROR si falla
+transporter.verify()
+  .then(() => {
+    console.log('✅ Sistema de correos conectado con éxito a Gmail');
+  })
+  .catch(err => {
+    // Si falla la red de Render, solo muestra una alerta, PERO NO apaga tu servidor
+    console.log('⚠️ Alerta de Correo: No se pudo conectar a Gmail al iniciar el servidor.');
+    console.log('Detalle del error de red:', err.message);
+  });
 
 module.exports = transporter;
